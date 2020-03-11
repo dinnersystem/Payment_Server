6b71310e9d42406e64e0330eae0f85ad94c04cdf
lawrence910426 <lawrence910426@gmail.com>
2020-03-11 14:45:05 +0800

    Fully connected with payment client/dinnersystem

--- Payment_Server/DS_Client.cs
+++ Payment_Server/DS_Client.cs
@@ -20,7 +20,7 @@ namespace Payment_Server
             {
                 byte[] temp = new byte[Int32.Parse(Properties.Resources.payload_len)];
                 this.client.Read(temp, 0, temp.Length);
-                payload.Append(Encoding.ASCII.GetString(temp));
+                payload.Append(Encoding.UTF8.GetString(temp));
             } while (this.client.DataAvailable);
             this.Payload = (JObject)JsonConvert.DeserializeObject(payload.ToString());
         }
--- Payment_Server/External_Client.cs
+++ Payment_Server/External_Client.cs
@@ -17,6 +17,9 @@ namespace Payment_Server
         NetworkStream client;
         Hashtable Response = Hashtable.Synchronized(new Hashtable());
         Queue Request = Queue.Synchronized(new Queue());
+        int work_id = 0;
+        object lock_obj = new object();
+
         public External_Client(TcpClient client)
         {
             this.client = client.GetStream();
@@ -29,10 +32,10 @@ namespace Payment_Server
         {
             if (Request.Count > 0)
             {
-                string s = Request.Dequeue() as string;
-                byte[] temp = new byte[Int32.Parse(Properties.Resources.payload_len)];
-                for (int i = 0; i < s.Length; i++) temp[i] = (byte)s[i];
-                client.Write(temp, 0, temp.Length);
+                byte[] buffer = new byte[Int32.Parse(Properties.Resources.payload_len)];
+                byte[] temp = Encoding.UTF8.GetBytes(Request.Dequeue() as string);
+                for (int i = 0; i < temp.Length; i++) buffer[i] = temp[i];
+                client.Write(buffer, 0, buffer.Length);
             }
         }
 
@@ -41,21 +44,26 @@ namespace Payment_Server
             byte[] temp = new byte[Int32.Parse(Properties.Resources.external_response_len)];
             client.Read(temp, 0, temp.Length);
             JObject response = (JObject)JsonConvert.DeserializeObject(Encoding.ASCII.GetString(temp));
-            JObject payload = response["payload"].ToObject<JObject>();
+            JObject payload = (JObject)response["payload"];
             if (response["type"].ToObject<string>() == "config") Config = payload;
             else
             {
-                string id = payload["org_id"].ToObject<string>();
-                if (!Response.ContainsKey(id)) throw new Exception("Received invalid id '" + id + "' from external client.");
-                (Response[id] as Action<string>)(payload["msg"].ToObject<string>());
+                string id = response["work_id"].ToObject<string>();
+                if (!Response.ContainsKey(id)) throw new Exception("Received invalid work id '" + id + "' from external client.");
+                (Response[id] as Action<string>)(JsonConvert.SerializeObject(payload));
                 Response.Remove(id);
             }
         }
 
         public void Run(JObject payload, Action<string> callback)
         {
-            Request.Enqueue(JsonConvert.SerializeObject(payload));
-            Response.Add(payload["org_id"].ToObject<string>(), callback);
+            lock (lock_obj)
+            {
+                payload["work_id"] = work_id.ToString();
+                Request.Enqueue(JsonConvert.SerializeObject(payload));
+                Response.Add(payload["work_id"].ToObject<string>(), callback);
+                work_id += 1;
+            }
         }
     }
 }
