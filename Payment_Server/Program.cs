600ff50bf236dd5964c9ad48f1c83bac5dcb17e8
Lawrence Wu <lawrence910426@gmail.com>
2020-03-16 07:16:17 +0800

    commit

--- Payment_Server/Program.cs
+++ Payment_Server/Program.cs
@@ -20,15 +20,18 @@ namespace Payment_Server
             var list = new ListView() { X = 3, Y = 2 , Width = Dim.Fill(2), Height = Dim.Fill(1) };
             win.Add(list);
 
-            Task.Run(() => { Application.Run(); });
-            while (true)
+            Task.Run(() =>
             {
-                Thread.Sleep(1000);
-                ArrayList clients = new ArrayList();
-                lock (core.ext_client_set) foreach (DictionaryEntry pair in core.ext_client_set) clients.Add("[ONLINE] External Client: " + pair.Key);
-                list.SetSource(clients);
-                Application.Refresh();
-            }
+                while (true)
+                {
+                    Thread.Sleep(1000);
+                    ArrayList clients = new ArrayList();
+                    lock (core.ext_client_set) foreach (DictionaryEntry pair in core.ext_client_set) clients.Add("[ONLINE] External Client: " + pair.Key);
+                    list.SetSource(clients);
+                    Application.Refresh();
+                }
+            });
+            Application.Run();
         }
     }
 }
