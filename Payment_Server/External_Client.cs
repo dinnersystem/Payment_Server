0d95c8cf34604ce711043bb40e7fa48c19a9d985
lawrence910426 <lawrence910426@gmail.com>
2020-03-22 12:27:55 +0800

    payment

--- Payment_Server/External_Client.cs
+++ Payment_Server/External_Client.cs
@@ -19,8 +19,7 @@ namespace Payment_Server
         Queue Work = Queue.Synchronized(new Queue());
         Action<string> dispose; bool has_disposed = false;
         JObject ping = new JObject();
-        int ping_interval = Int32.Parse(Properties.Resources.ping_interval) , work_interval = Int32.Parse(Properties.Resources.work_interval);
-
+        int ping_interval = Int32.Parse(Properties.Resources.ping_interval), work_interval = Int32.Parse(Properties.Resources.work_interval);
         public External_Client(TcpClient client, Action<string> dispose)
         {
             client.NoDelay = true; this.client = client.GetStream(); this.dispose = dispose; ping["operation"] = "ping";
@@ -28,38 +27,42 @@ namespace Payment_Server
             Task.Run(() => { for (; !has_disposed; Thread.Sleep(work_interval)) Run_Work(false); });
             Task.Run(() => { for (; !has_disposed; Thread.Sleep(ping_interval)) Run(ping, (string s) => { }); });
         }
-
         void Dispose() { if (!has_disposed) { dispose(ID); has_disposed = true; } }
-
         void Run_Work(bool config)
         {
             try
             {
                 while (config || Work.Count > 0)
                 {
-                    Tuple<object ,object> item = new Tuple<object, object>(null ,null);
+                    Tuple<object, object> item = new Tuple<object, object>(null, null);
                     if (!config)
                     {
                         item = Work.Dequeue() as Tuple<object, object>;
-                        byte[] buffer = new byte[Int32.Parse(Properties.Resources.payload_len)] ,temp = Encoding.UTF8.GetBytes(item.Item1 as string);
+                        byte[] buffer = new byte[Int32.Parse(Properties.Resources.payload_len)], temp = Encoding.UTF8.GetBytes(item.Item1 as string);
                         for (int i = 0; i < temp.Length; i++) buffer[i] = temp[i];
                         client.Write(buffer, 0, buffer.Length);
                     }
                     byte[] receive = new byte[Int32.Parse(Properties.Resources.external_response_len)];
                     client.Read(receive, 0, receive.Length);
-                    JObject response = (JObject)JsonConvert.DeserializeObject(Encoding.UTF8.GetString(receive));
-                    JObject payload = (JObject)response["payload"];
-                    if (config) { Config = payload; ID = Config["org_id"].ToObject<string>(); config = false; }
-                    else (item.Item2 as Action<string>)(JsonConvert.SerializeObject(payload));
+                    string ans = "";
+                    try
+                    {
+                        JObject response = (JObject)JsonConvert.DeserializeObject(Encoding.UTF8.GetString(receive));
+                        JObject payload = (JObject)response["payload"];
+                        ans = JsonConvert.SerializeObject(payload) as string;
+                        if (config) { Config = payload; ID = Config["org_id"].ToObject<string>(); }
+                    }
+                    catch (Exception e) { ans = e.Message + " " + e.StackTrace; }
+                    if (config) config = false; 
+                    else (item.Item2 as Action<string>)(ans);
                 }
             }
             catch (Exception e)
-            { 
-                Console.WriteLine(e.Message + "\n" + e.StackTrace); 
-                Dispose(); 
+            {
+                Console.WriteLine(e.Message + "\n" + e.StackTrace);
+                Dispose();
             }
         }
-
         public void Run(JObject payload, Action<string> callback) { Work.Enqueue(new Tuple<object, object>(JsonConvert.SerializeObject(payload), callback)); }
     }
 }
