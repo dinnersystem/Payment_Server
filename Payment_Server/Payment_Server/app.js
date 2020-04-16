5f7a99fdf46335a240cac453ce4b8ba15dffe054
lawrence910426 <lawrence910426@gmail.com>
2020-04-16 23:46:58 +0800

    Bulk IO

---
+++ Payment_Server/Payment_Server/app.js
@@ -0,0 +1,75 @@
+const net = require('net');
+const express = require('express')
+const bodyParser = require('body-parser');
+const fs = require('fs')
+const logger = fs.createWriteStream('log.txt', { flags: 'a' })
+const moment = require('moment')
+
+function log(msg) {
+	var output = moment().format("YYYY-MM-DD hh:mm:ss") + "," + msg + "\n"
+	logger.write(output);
+	console.log(output);
+}
+function response_DS(work) {
+	log("EXT," + JSON.stringify(work))
+	callbacks[work.org_id][work.work_id](work.msg);
+	delete events[work.org_id][work.work_id]
+	delete callbacks[work.org_id][work.work_id]
+}
+
+var events = {
+	"1": {}
+};
+var callbacks = {
+	"1": {}
+}
+
+
+var work_id = 0;
+var server = net.createServer(function (socket) {
+	socket.on('data', function (data) {
+		log("DS," + data)
+		var json = JSON.parse(data)
+		var wid = work_id++;
+		json.work_id = wid
+		events[json.org_id][wid] = json
+		callbacks[json.org_id][wid] = function (msg) {
+			socket.write(JSON.stringify(msg))
+			socket.end();
+		}
+		setTimeout(() => {
+			response_DS({
+				org_id: json.org_id,
+				work_id: wid,
+				msg: "Timeout"
+            })
+        }, 5000)
+	});
+});
+server.listen(1101, '0.0.0.0');
+
+
+
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.get('/show_work', function (req, res) {
+	/* var resp = {}
+	var count = 0
+	for (var key in events[req.query.org_id]) {
+		if (dictionary.hasOwnProperty(key)) resp[key] = events[req.query.org_id][key]
+		if (count > 100) break
+	}
+	res.send(JSON.stringify(resp)); */
+	res.send(JSON.stringify(events[req.query.org_id]))
+});
+app.post('/submit_work', function (req, res) {
+	req.body.forEach((work) => {
+		if (events[work.org_id][work.work_id] == undefined) { return }
+		response_DS(work)
+    })
+	res.send("OK")
+});
+app.listen(5269, function () {
+	log('Payment Server is now listening!');
+});
