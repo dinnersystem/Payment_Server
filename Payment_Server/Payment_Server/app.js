28f34e40780f486d54391fcae22227e28844ccd2
lawrence910426 <lawrence910426@gmail.com>
2020-04-17 10:34:10 +0800

    restart

--- Payment_Server/Payment_Server/app.js
+++ Payment_Server/Payment_Server/app.js
@@ -4,8 +4,6 @@ const bodyParser = require('body-parser');
 const fs = require('fs')
 const logger = fs.createWriteStream('log.txt', { flags: 'a' })
 const moment = require('moment')
-const AsyncLock = require('async-lock');
-const lock = new AsyncLock();
 
 function log(msg) {
 	var output = moment().format("YYYY-MM-DD hh:mm:ss") + "," + msg + "\n"
@@ -20,18 +18,15 @@ var callbacks = {
 	"1": {}
 }
 function response_DS(work) {
-	return new Promise(async function (res) {
-		await lock.acquire('key', function (done) {
-			if (events[work.org_id][work.work_id] != undefined) {
-				log("EXT_RESP," + JSON.stringify(work))
-				callbacks[work.org_id][work.work_id](work.msg);
-				delete events[work.org_id][work.work_id]
-				delete callbacks[work.org_id][work.work_id]
-				done()
-			}
-		});
+	return new Promise((res) => {
+		if (events[work.org_id][work.work_id] != undefined) {
+			log("EXT_RESP," + JSON.stringify(work))
+			callbacks[work.org_id][work.work_id](work.msg);
+			delete events[work.org_id][work.work_id]
+			delete callbacks[work.org_id][work.work_id]
+		}
 		res()
-    })
+	})
 }
 
 
@@ -39,9 +34,17 @@ function response_DS(work) {
 var work_id = 0;
 var server = net.createServer(function (socket) {
 	socket.on('data', function (data) {
+		log("DS_REQ," + data)
 		var json = JSON.parse(data)
 		var wid = work_id++;
 		json.work_id = wid
+		events[json.org_id][wid] = json
+		callbacks[json.org_id][wid] = function (msg) {
+			msg = JSON.stringify(msg)
+			log("DS_RESP," + msg)
+			socket.write(msg)
+			socket.end();
+		}
 		setTimeout(() => {
 			response_DS({
 				org_id: json.org_id,
@@ -49,17 +52,6 @@ var server = net.createServer(function (socket) {
 				msg: { error: "Timeout" }
 			})
 		}, 15000)
-		await lock.acquire('key', function (done) {
-			events[json.org_id][wid] = json
-			callbacks[json.org_id][wid] = function (msg) {
-				msg = JSON.stringify(msg)
-				log("DS_RESP," + msg)
-				socket.write(msg)
-				socket.end();
-			}
-			done()
-		});
-		log("DS_REQ," + data)
 	});
 });
 server.listen(1101, '0.0.0.0');
@@ -70,17 +62,14 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.get('/show_work', function (req, res) {
-	log("EXT_REQ," + JSON.stringify(req.query))
-	lock.acquire('key', function (done) {
-		res.send(JSON.stringify(events[req.query.org_id]))
-		done()
-	});
+	log("EXT_REQ," + req.query)
+	res.send(JSON.stringify(events[req.query.org_id]))
 });
 app.post('/submit_work', function (req, res) {
 	Promise.all(req.body.map((work) => {
 		return response_DS(work)
 	})).then(() => {
 		res.send("OK")
-    })
+	})
 });
 app.listen(5269, function () { log('Payment Server is now listening!'); });
